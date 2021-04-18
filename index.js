26ebe5ba7587cf3500370553825c4e84c4e1e59f
simokh <m.simokhouili@gmail.com>
2021-04-18 16:05:10 -0400

    loading attributes to the page

--- index.js
+++ index.js
@@ -3,11 +3,27 @@ const endPoint = "http://localhost:3000/api/v1/museums"
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    fetch(endPoint)
-    .then(res => res.json())
-    .then(museums => {
-        console.log(museums);
+    getMuseum()
+
     })
+
+    function getMuseum() {
+        fetch(endPoint)
+        .then(res => res.json())
+        .then(museums => {
+            museums.data.forEach(museum => {
+                const museumsMarkup = `
+                <div data-id=${museum.id}>
+                <img src=${museum.attributes.img_url} 
+                height= "200" width="250">
+                <h3>${museum.attributes.name}</h3>
+                <h4>${museum.attributes.borough}</h4>
+                <button data-id=${museum.id}>edit</button>
+                </div>
+                <br><br>`;
+                document.querySelector('#museum-container').innerHTML += museumsMarkup
+                
+            })
+    })
+}
     
-    
-})
