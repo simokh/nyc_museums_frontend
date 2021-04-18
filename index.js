401ce3f82f0237d3909ee3835fdc02e7fe87e5ed
simokh <m.simokhouili@gmail.com>
2021-04-18 15:28:52 -0400

    loading the Dom and fetch requests are on

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("im good")
+const endPoint = "http://localhost:3000/api/v1/museums"
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    fetch(endPoint)
+    .then(res => res.json())
+    .then(museums => {
+        console.log(museums);
+    })
+    
+    
+})
