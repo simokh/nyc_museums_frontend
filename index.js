0a32feffaa891c6233c896d027bfe9c0cc4b2977
simokh <m.simokhouili@gmail.com>
2021-04-19 01:09:26 -0400

    adding aattri to the page

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
 const endPoint = "http://localhost:3000/api/v1/museums"
+const endPoint2 = "http://localhost:3000/api/v1/reviews" 
 
 
 document.addEventListener('DOMContentLoaded', () => {
 
     getMuseum()
-
+    getReview()
     })
 
     function getMuseum() {
         fetch(endPoint)
         .then(res => res.json())
         .then(museums => {
+            // debugger
             museums.data.forEach(museum => {
                 const museumsMarkup = `
                 <div data-id=${museum.id}>
                 <img src=${museum.attributes.img_url} 
-                height= "200" width="250">
+                height= "200" width="">
                 <h3>${museum.attributes.name}</h3>
                 <h4>${museum.attributes.borough}</h4>
                 <button data-id=${museum.id}>edit</button>
                 </div>
-                <br><br>`;
+                <br>`;
                 document.querySelector('#museum-container').innerHTML += museumsMarkup
                 
             })
     })
 }
+
+function getReview(){
+    fetch(endPoint2)
+    .then(res => res.json())
+    .then(reviews => {
+        reviews.data.forEach(review =>{
+            const reviewsMarkup = `
+            <div data-id=${review.id}> 
+            <h3>Review: ${review.attributes.review}</h3>
+            <h4>Rating: ${review.attributes.rating}</h4>
+            <button data-id=${review.id}>create</button>
+
+            </div>    
+            <br>`; 
+            document.querySelector('#review-container').innerHTML += reviewsMarkup
+
+                    })       
+
+                })
+            }
     
