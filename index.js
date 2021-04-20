154c4100dc9b889326baa413d2f7dd6a9c2cc20a
simokh <m.simokhouili@gmail.com>
2021-04-20 15:14:42 -0400

    working on the post request

--- index.js
+++ index.js
@@ -1,12 +1,70 @@
-const endPoint = "http://localhost:3000/api/v1/museums"
-const endPoint2 = "http://localhost:3000/api/v1/reviews" 
+const endPoint = "http://localhost:3000/api/v1/museums";
+const endPoint2 = "http://localhost:3000/api/v1/reviews"
 
 
 document.addEventListener('DOMContentLoaded', () => {
 
     getMuseum()
     getReview()
+    // postReview()
+
+    const writeReview = document.querySelector("#write-review-form")
+    
+    writeReview.addEventListener("submit", (e) => formHandler(e))
+        // debugger
+
     })
+
+    function formHandler(e) {
+        e.preventDefault()
+        console.log("i am working");
+
+        const rev = document.querySelector('#input-review').value
+        const rate = document.querySelector('#rating').value
+        const museum = document.querySelector('#museum_id').value
+
+
+        postReview(rev, rate, museum)
+    }
+
+    function postReview(rev, rate, museum) {
+        // confirm these values are coming through properly
+        console.log(rev, rate, museum);
+        // build body object
+        // let bodyData = {rev, rate, museum}
+      
+        fetch(endPoint2, {
+        //   // POST request
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json"
+
+            },
+          body: JSON.stringify({
+              rev,
+              rate,
+              museum
+          })
+            })
+            .then(response => response.json())
+            .then(review_museum => {
+                console.log(review_museum);
+    //             const reviewData = review_museum.data
+    //     //   // render JSON response
+    //             const reviewMarkup = `
+    //             <div data-id=${review_museum.id}> 
+    //                 <h3>Review: ${reviewData.attributes.review}</h3>
+    //                 <h4>Rating: ${reviewData.attributes.rating}</h4>
+    //                 <button data-id=${reviewData.id}>delete</button>
+    //     //   </div>
+    //     //   <br><br>`;
+      
+    //       document.querySelector('#review-container').innerHTML += reviewMarkup;
+        })
+      }
+    
+
 
     function getMuseum() {
         fetch(endPoint)
@@ -45,7 +103,10 @@ function getReview(){
             document.querySelector('#review-container').innerHTML += reviewsMarkup
 
                     })       
+           
+                })            
+        
+        }
+        
 
-                })
-            }
     
