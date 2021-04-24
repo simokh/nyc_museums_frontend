2b40ea1c821b07fce29383a8aace577494bf92ac
simokh <m.simokhouili@gmail.com>
2021-04-23 23:31:33 -0400

    factor the index file into a class Review OOJ

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const endPoint2 = "http://localhost:3000/api/v1/reviews"
 document.addEventListener('DOMContentLoaded', () => {
 
     // getMuseum()
-    // getReview()
+    getReview()
 
 
     const writeReview = document.querySelector("#write-review-form")
@@ -48,35 +48,51 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(review => {
             console.log(review);
 
-                const reviewData = review.data
+                const reviewData = review.data.attributes
         //   // render JSON response
-                const reviewMarkup = `
-                <div data-id=${review.id}>
-                    <img src=${reviewData.attributes.museum.img_url} height= "200" width="">
-                    <h4>Musuem: ${reviewData.attributes.museum.name}</h4>
-                    <h3>Review: ${reviewData.attributes.review_post}</h3>
-                    <h3>Rating: ${reviewData.attributes.rating}</h3>
-                    <button data-id=${reviewData.id}>delete</button>
-           </div>
-           <br><br>`;
+        //         const reviewMarkup = `
+        //         <div data-id=${review.id}>
+        //             <img src=${reviewData.museum.img_url} height= "200" width="">
+        //             <h4>Musuem: ${reviewData.museum.name}</h4>
+        //             <h3>Review: ${reviewData.review_post}</h3>
+        //             <h3>Rating: ${reviewData.rating}</h3>
+        //             <button data-id=${reviewData.id}>delete</button>
+        //    </div>
+        //    <br><br>`;
+
+                const newReview = new Review(reviewData.id, reviewData)
+                document.querySelector('#review-container').innerHTML += newReview.renderReviewPost()
       
-          document.querySelector('#review-container').innerHTML += reviewMarkup;
-            document.getElementById('write-review-form').reset();
+                document.querySelector('#review-container').innerHTML += reviewMarkup;
+                document.getElementById('write-review-form').reset();
 
         })
 
     }
 
 
+function getReview(){
+    fetch(endPoint2)
+    .then(res => res.json())
+    .then(reviews => {
+        reviews.data.forEach(review =>{
+            // const reviewsMarkup = `
+            // <div data-id=${review.id}>
+            // <img src=${review.attributes.museum.img_url} height= "200" width="">
+            // <h3>Review: ${review.attributes.review_post}</h3>
+            // <h4>Rating: ${review.attributes.rating}</h4>
+            // <button data-id=${review.id}>delete</button>
 
+            // </div>    
+            // <br>`; 
+            const newReview = new Review(review.id, review.attributes)
+            document.querySelector('#review-container').innerHTML += newReview.renderReviewPost()
 
-
-
-
-
-
-
-
+        })       
+           
+    })            
+        
+}
 
 
 
@@ -85,13 +101,13 @@ document.addEventListener('DOMContentLoaded', () => {
 //         .then(res => res.json())
 //         .then(museums => {
 //             // debugger
-//             museums.data.forEach(museum => {
+//             museums.data.forEach(museum =>  {
 //                 const museumsMarkup = `
 //                 <div data-id=${museum.id}>
 //                 <img src=${museum.attributes.img_url} 
 //                 height= "200" width="">
-//                 <h3>${museum.attributes.name}</h3>
-//                 <h4>${museum.attributes.borough}</h4>
+//                 <h4>${museum.attributes.name}</h4>
+//                 <h3>${museum.attributes.borough}</h3>
 //                 </div>
 //                 <br>`;
 //                 document.querySelector('#museum-container').innerHTML += museumsMarkup
@@ -100,27 +116,7 @@ document.addEventListener('DOMContentLoaded', () => {
 //     })
     
 // }
-
-// function getReview(){
-//     fetch(endPoint2)
-//     .then(res => res.json())
-//     .then(reviews => {
-//         reviews.data.forEach(review =>{
-//             const reviewsMarkup = `
-//             <div data-id=${review.id}> 
-//             <h3>Review: ${review.attributes.review_post}</h3>
-//             <h4>Rating: ${review.attributes.rating}</h4>
-//             <button data-id=${review.id}>delete</button>
-
-//             </div>    
-//             <br>`; 
-//             document.querySelector('#review-container').innerHTML += reviewsMarkup
-
-//         })       
-           
-//     })            
-        
-// }
         
 
     
+// 
--- src/review.js
+++ src/review.js
@@ -1,11 +1,27 @@
-// class Musuem {
+class Review {
+    constructor(id, reviewAttri) {
+        this.id = id;
+        // debugger 
+        this.img_url = reviewAttri.museum.img_url
+        this.museum = reviewAttri.museum.name
+        this.review_post = reviewAttri.review_post
+        this.rating = reviewAttri.rating
+        Review.all.push(this)
+    }
 
-//     constructor(museum, museumAttr) {
+    renderReviewPost() {
+        return `
+        <div data-id=${this.id}>
+        <img src=${this.img_url} height= "200" width="">
+        <h4>Musuem: ${this.museum}</h4>
+        <h3>Review: ${this.review_post}</h3>
+        <h3>Rating: ${this.rating}</h3>
+        <button data-id=${this.id}>delete</button>
+        <br><br>`; 
+    }
+}
 
-//         this.id = museum.id 
-//         this.img_url = museum.img_url
-        
-//     }
+Review.all = [];
 
 
 
@@ -14,7 +30,4 @@
 
 
 
-
-
-// }
 
